0fc0fda8d13f6d5057e400539bc67de39f8ac1cc
Juan Gomez <camilo12378@gmail.com>
2023-03-08 13:59:36 -0500

    header y card

--- src/screens/StartGameScreen.jsx
+++ src/screens/StartGameScreen.jsx
@@ -1,27 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
-import Card from '../components/Card'
-import React from 'react'
+import Card from "../components/Card";
+import React from "react";
 
 const StartGameScreen = () => {
   return (
     <View>
+      <Text style={styles.title}>Comenzar Juego</Text>
       <View style={styles.cardContainer}>
         <Card style={styles.card}>
-          <Text>Hola</Text>
+          <Text style={styles.titleCard}>Elije un número</Text>
+          <TextInput style={styles.inputCard} />
         </Card>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default StartGameScreen
+export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    cardContainer:{
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop:10,
-      },
-      card:{backgroundColor:"white"}
-})
+  title: {
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 20,
+  },
+  cardContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  card: {
+    backgroundColor: "white",
+  },
+  titleCard: {
+    textAlign: "center",
+  },
+  inputCard: {
+    padding:0,
+    margin:0,
+    height:50,
+    borderBottomWidth:1,
+    borderBottomColor:'black',
+  },
+});
