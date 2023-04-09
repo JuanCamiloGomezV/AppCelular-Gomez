9a6148bbd18818540bf95772a986e967b53a8c8f
Juan Gomez <camilo12378@gmail.com>
2023-04-09 09:16:34 -0500

    login

--- src/data/Database.js
+++ src/data/Database.js
@@ -1,3 +1,6 @@
 export const URL_API = 'https://mascotalandia-78836-default-rtdb.firebaseio.com/'
 export const SIGN_UP_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA0MNBXObnFp8VRa20aizpOa47qJK4JLvs'
 export const SIGN_IN_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA0MNBXObnFp8VRa20aizpOa47qJK4JLvs'
+
+//API
+const a = ''
